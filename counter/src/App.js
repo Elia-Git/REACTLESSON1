e60c9266485e1b89586dda9a5982fee19591983a
Elia-Git <holeselv17@gmail.com>
2020-09-27 01:52:48 +0200

    files modified

--- counter/src/App.js
+++ counter/src/App.js
@@ -1,38 +1,33 @@
-import React from "react";
-import Counter from "./Counter";
-import CounterClass from "./CounterClassBased";
-import "./App.css";
+import React from 'react'
+import Counter from './Counter'
+import CounterClass from './CounterClassBased'
+import './App.css'
 
 // Functional type component
-const MyName = (props) => {
-    console.log(props);
+const MyName = props => {
+  console.log(props)
 
-    return <div > My name is { props.name } < /div>;
-};
+  return <div> My name is {props.name} </div>
+}
 
 class MySecondName extends React.Component {
-    render() {
-        return <div > My Second Name is { this.props.name } < /div>;
-    }
+  render () {
+    return <div> My Second Name is {this.props.name} </div>
+  }
 }
 
-const names = ["Elias", "Anil", "Deepti"];
+const names = ['Elias']
 
-function App() {
-    return ( <
-        div className = "App" > { " " } {
-            names.map((name) => ( <
-                MyName key = { name }
-                name = { name }
-                />
-            ))
-        } { " " } <
-        MySecondName name = "Weldemichael" / >
-        <
-        Counter countFrom = { 0 }
-        /> <CounterClass countFrom={0} / > { " " } <
-        /div>
-    );
+function App () {
+  return (
+    <div className='App'>
+      {names.map(name => (
+        <MyName key={name} name={name} />
+      ))}
+      <MySecondName name='Weldemichael' />
+      <Counter countFrom={0} /> <CounterClass countFrom={0} />
+    </div>
+  )
 }
 
-export default App;
+export default App
--- counter/src/Counter.js
+++ counter/src/Counter.js
@@ -1,16 +1,14 @@
-import React, { useState } from "react";
+import React, { useState } from 'react'
 
-const Counter = (props) => {
-    const [myCount, setMyCount] = useState(props.countFrom);
-    // const array = [0, () => {}]
-    // const [name, setName] = useState('Elias')
-    return ( <
-        div >
-        <
-        button onClick = {
-            () => setMyCount(myCount + 1) } > { myCount } < /button>{" "} <
-        /div>
-    );
-};
+const Counter = props => {
+  const [myCount, setMyCount] = useState(props.countFrom)
+  // const array = [0, () => {}]
+  // const [name, setName] = useState('Elias')
+  return (
+    <div>
+      <button onClick={() => setMyCount(myCount + 1)}> {myCount} </button>{' '}
+    </div>
+  )
+}
 
-export default Counter;
+export default Counter
